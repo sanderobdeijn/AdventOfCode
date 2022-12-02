6705a5ac2392f13389796233af7cf5b91989c3c4
Sander Obdeijn <info@sanderobdeijn.nl>
2022-12-02 15:12:57 +0100

    Add Day 00 to 2021

---
+++ src/AdventOfCode.Year2021.UnitTests/Day00.cs
@@ -0,0 +1,38 @@
+using AdventOfCode.Year2021.Day00;
+
+namespace AdventOfCode.Year2021.UnitTests;
+
+public class Day00
+{
+    [Fact]
+    public void Challenge1ExampleShouldBeCorrect()
+    {
+        var sut = new Challenge1Example();
+
+        Assert.Equal(1, sut.Solve());
+    }
+
+    [Fact]
+    public void Challenge1InputShouldBeCorrect()
+    {
+        var sut = new Challenge1Input();
+
+        Assert.Equal(1, sut.Solve());
+    }
+
+    [Fact]
+    public void Challenge2ExampleShouldBeCorrect()
+    {
+        var sut = new Challenge2Example();
+       
+        Assert.Equal(1, sut.Solve());
+    }
+    
+    [Fact]
+    public void Challenge2InputShouldBeCorrect()
+    {
+        var sut = new Challenge2Input();
+
+        Assert.Equal(1, sut.Solve());
+    }
+}
